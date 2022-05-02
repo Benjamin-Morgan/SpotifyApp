514874b765f36c96ec58cbc0ebac424acd19d3f0
Benjamin Morgan <benjaminmorgan117@gmail.com>
2022-05-02 08:15:17 -0700

    add API controller, button styling

--- app.js
+++ app.js
@@ -11,11 +11,33 @@ const APIController = (function(){
         const result = await fetch('https://accounts.spotify.com/api/token', {
             method: 'POST',
             headers: {
-                // taken from api docs
+                // taken from Spotify api docs
                 'Content-Type' : 'application/x-www-form-urlencoded',
                 'Authorization' : 'Basic ' + btoa(clientId + ':' + clientSecret)
             },
             body: 'grant_type=client_credentials'
+        });
+
+        const data = await result.json();
+        return data.access_token;
+    }
+
+    const _getGenres = async (token) => {
+        const result = await fetch(`https://api.spotify.com/v1/browse/categories?locale=sv_US`, {
+            method: 'GET',
+            headers: { 'Authorization' : 'Bearer ' + token}
+        });
+
+        const data = await result.json();
+        return data.categories.items;
+    }
+
+    const _getPlaylistByGenre = async (token, genreID) => {
+        const limit = 10;
+
+        const result = await fetch(`https://api.spotify.com/v1/browse/categories/${genreId}/playlists?limit=${limit}`, {
+            method: 'GET',
+            headeres: { 'Authorization' : 'Bearer ' + token}
         })
     }
 })
